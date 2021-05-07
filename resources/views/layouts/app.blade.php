a320e8ef001e45184a4046f7419c6b6e2f2050a0
Olivier Huvelle <olivier.huvelle@gmail.com>
2021-05-07 17:07:30 +0200

    switching to fm project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>LaravelOverview | @yield('title')</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
         <main>
